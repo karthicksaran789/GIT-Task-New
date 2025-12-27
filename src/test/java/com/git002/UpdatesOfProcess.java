ce5c333c49be0ad6cc187c31ba3aa0ee4f87991a
KarthickSaran <karthicksaran789@gmail.com>
2025-12-27 11:17:10 +0530

    Updated 22-39

--- src/test/java/com/git002/UpdatesOfProcess.java
+++ src/test/java/com/git002/UpdatesOfProcess.java
@@ -18,5 +18,24 @@ public static void main(String[] args) {
 	System.out.println("java");
 	System.out.println("java");
 	System.out.println("java");
+	
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
+	System.out.println("Selenium");
 }
 }
