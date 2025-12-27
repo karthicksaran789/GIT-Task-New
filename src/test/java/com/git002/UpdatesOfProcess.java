15510a01e48d1d5c8c5f3f3535cb94b8530559bb
KarthickSaran <karthicksaran789@gmail.com>
2025-12-27 10:58:13 +0530

    Intial commit-01

---
+++ src/test/java/com/git002/UpdatesOfProcess.java
@@ -0,0 +1,22 @@
+package com.git002;
+
+public class UpdatesOfProcess {
+public static void main(String[] args) {
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+	System.out.println("java");
+}
+}
